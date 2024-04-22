c31a420dbf7724a0cf2369b732de999da396fb09
JhonMercedes <jhonzeras93@gmail.com>
2024-04-22 16:50:32 -0300

    resolvendo desafios python AI Backend Developer

---
+++ exercicios/plano-internet.py
@@ -0,0 +1,17 @@
+
+consumo = float(input())
+
+def recomendar_plano(consumo):
+    if consumo <= 10:
+        print("Plano Essencial Fibra - 50Mbps")
+    elif consumo <= 20:
+        print("Plano Prata Fibra - 100Mbps")
+    else:
+        print("Plano Premium Fibra - 300Mbps")
+    
+
+recomendar_plano(consumo)
+
+
+
+
